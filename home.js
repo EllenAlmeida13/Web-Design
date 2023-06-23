a04469f24834ebf52f06925e3c7124486efa01db
Ellen Almeida <ruthgaah13@gmail.com>
2023-06-23 14:35:03 -0300

    jxdy

---
+++ home.js
@@ -0,0 +1,13 @@
+// Declarando a variável let
+let nome = "João";
+
+// Atribuindo um valor à variável
+
+// Imprimindo o valor da variável no console
+console.log(nome);
+
+// Atualizando o valor da variável
+let idade = 30;
+
+// Imprimindo o novo valor da variável no console
+console.log(idade);
