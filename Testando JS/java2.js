a5a24a7318f95377823ad71d7545bd1b73b3a56e
Ellen Almeida <ruthgaah13@gmail.com>
2023-06-27 13:51:22 -0300

    udut

--- Testando JS/java2.js
+++ Testando JS/java2.js
@@ -5,66 +5,47 @@ console.log("Olá, mundo!");
 var paragrafo = document.querySelector("p");
 paragrafo.style.color = "red";
 
-// Testando Códigos de ontem
-
-let frutas = ["Maçã", "Banana", "Laranja", "Morango", "Abacaxi"];
-
-//console.log(frutas); // Exibe o array completo no console
-
-//console.log(frutas[0]); // Exibe a primeira fruta do array (Maçã)
-
-//frutas.push("Pera"); // Adiciona a fruta Pera ao final do array
-
-//console.log(frutas); // Exibe o array atualizado
-
-//console.log(frutas.length); // Exibe o tamanho do array (quantidade de frutas)
-
-//frutas.pop(); // Remove a última fruta do array (Pera)
-
-//console.log(frutas); // Exibe o array atualizado
-
-
 //
 
 // Obtendo o elemento pelo ID
-//let elemento = document.getElementById("meuElemento");
+let elemento = document.getElementById("meuElemento");
 
 // Adicionando o evento de clique ao elemento
-//elemento.addEventListener("click", function() {
-    //alert("Clicou no elemento!");
-//});
+elemento.addEventListener("click", function() {
+    alert("Clicou no elemento!");
+});
 
 //
 
 // Obtendo o elemento pelo ID
-//let info = document.getElementById("mye");
+let info = document.getElementById("mye");
 
 // Alterando o conteúdo do elemento
-//info.innerHTML = "Não voltem KKKKKK";
+info.innerHTML = "Não voltem KKKKKK";
 
 //
 
 
 //
 
-//document.getElementById("form-contato").addEventListener("submit", function(event) {
-    //event.preventDefault(); // Impede o envio padrão do formulário
+document.getElementById("form-contato").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
   
 // Obtenção dos valores dos campos
-//let nome = document.getElementById("nome").value;
-//let email = document.getElementById("email").value;
-//let mensagem = document.getElementById("mensagem").value;
+let nome = document.getElementById("nome").value;
+let email = document.getElementById("email").value;
+let mensagem = document.getElementById("mensagem").value;
   
 // Montagem do corpo do email
-//let corpoEmail = "Nome: " + nome + "\n";
-//orpoEmail += "Email: " + email + "\n";
-//corpoEmail += "Mensagem: " + mensagem;
+let corpoEmail = "Nome: " + nome + "\n";
+corpoEmail += "Email: " + email + "\n";
+corpoEmail += "Mensagem: " + mensagem;
   
 // Simulação do envio do email
-//console.log(corpoEmail);
-//alert("Email enviado com sucesso!");
+console.log(corpoEmail);
+alert("Email enviado com sucesso!");
   
 // Limpa os campos do formulário
-//document.getElementById("form-contato").reset();
-//});
+document.getElementById("form-contato").reset();
+});
   
---
+++ script.js
@@ -0,0 +1,68 @@
+// Criando um array de frutas
+//let frutas = ["Maçã", "Banana", "Laranja", "Abacaxi", "Melancia"];
+
+// Imprimindo o array no console
+//console.log(frutas);
+
+let numero = 0;
+
+   if (0 === "0") {
+    console.log("Os dois são iguais")
+
+   } else { (0 != "0") 
+    console.log("Os dois são DIFERENTES!!")
+   }
+
+
+// Acessando um elemento específico do array
+//let terceiraFruta = frutas[2];
+//console.log("A terceira fruta é: " + terceiraFruta);
+
+// Modificando um elemento do array
+//frutas[1] = "Romã";
+//console.log("Após a modificação, o array de frutas fica: " + frutas);
+
+// Adicionando um novo elemento ao final do array
+//frutas.push("Morango");
+//console.log("Após adicionar o morango, o array de frutas fica: " + frutas);
+
+// Removendo o último elemento do array
+//let ultimaFruta = frutas.pop();
+//console.log("A última fruta removida foi: " + ultimaFruta);
+//console.log("Após remover o último elemento, o array de frutas fica: " + frutas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Declarando a variável let
+//let nome = "João";
+
+// Atribuindo um valor à variável
+
+// Imprimindo o valor da variável no console
+//console.log(nome);
+
+// Atualizando o valor da variável
+//let idade = 30;
+
+// Imprimindo o novo valor da variável no console
+//console.log(idade);
