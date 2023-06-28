68ea211130d8d04b51477402b18a26d29c839cc7
Ellen Almeida <ruthgaah13@gmail.com>
2023-06-28 14:51:55 -0300

    djte

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
+function fazerLogin (){
+   //valores de entrada do formulario
+   let email = document.getElementById("email").value;
+
+   let senha = document.getElementById ("senha").value;
+
+   // como validar o email e senha 
+
+   if (email === "email" && senha === "senha") {
+       // redirecionar para a pagina principal
+       window.location.href = "homedositeparallel.html";
+
+   } else  { 
+           (email != "email" && senha != "senha")
+           alert("sua entrada não foi autorizada")
+       }
+}
+
+
+
+
+
+
+
 // Criando um array de frutas
 //let frutas = ["Maçã", "Banana", "Laranja", "Abacaxi", "Melancia"];
 
 // Imprimindo o array no console
 //console.log(frutas);
 
-let numero = 0;
+//let numero = 0;
 
-   if (0 === "0") {
-    console.log("Os dois são iguais")
+   //if (0 === "0") {
+    //console.log("Os dois são iguais")
 
-   } else { (0 != "0") 
-    console.log("Os dois são DIFERENTES!!")
-   }
+   //} else { (0 != "0") 
+   // console.log("Os dois são DIFERENTES!!")
+   //}
 
 
 // Acessando um elemento específico do array
